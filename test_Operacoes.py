f48ffacd54a1e7aaf7f451238839be38258fd328
Paulo Elias <paulo.elias@fatec.sp.gov.br>
2026-08-26 20:43:09 -0300

    primeiro commit, com pipe e calculadora

---
+++ test_Operacoes.py
@@ -0,0 +1,15 @@
+import pytest
+
+from calculadora import soma, sub, multi, divi
+
+def test_soma():
+    assert soma(5, 5) == 10
+
+def test_sub():
+    assert sub(2, 6) == 8
+
+def test_multi():
+    assert multi(4, 2)== 8
+
+def test_divi():
+    assert divi(10, 2)== 5
