20ec7192f6cd8df0e3d489a9694a6308925042fe
Paulo Elias <paulo.elias@fatec.sp.gov.br>
2026-08-26 21:25:03 -0300

    correcao subtraca

--- test_Operacoes.py
+++ test_Operacoes.py
@@ -5,11 +5,14 @@ from calculadora import soma, sub, multi, divi
 def test_soma():
     assert soma(5, 5) == 10
 
+
 def test_sub():
     assert sub(2, 6) == 4
 
+
 def test_multi():
     assert multi(4, 2) == 8
+
 
 def test_divi():
     assert divi(10, 2) == 5
